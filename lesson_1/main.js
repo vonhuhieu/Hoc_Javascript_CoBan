1e82ec0496f2b07a695fcf8055e364182a0b1b92
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-04-29 08:45:49 +0700

    message là tham số, 'Hi anh em F8' là đối số

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,3 +1,9 @@
 let result = "A" && "B" && "C";
 let resultBackUp = "A" || "B" || "C" || "D";
 console.log(resultBackUp);
+
+function showMessage(message) {
+    console.log(message);
+}
+
+showMessage("Hi anh em F8");
