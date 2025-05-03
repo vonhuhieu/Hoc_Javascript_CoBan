5faea559bc1e4cbfaf13a2fbac3a687150438c3f
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-03 10:25:54 +0700

    Toán tử so sánh

---
+++ 3. Toán tử, kiểu dữ liệu/Toán tử so sánh/Toán tử so sánh_main.js
@@ -0,0 +1,14 @@
+let a = 3, b = 5;
+if (a <= b) {
+    console.log("Điều kiện đúng");
+} else {
+    console.log("Điều kiện sai");
+}
+
+let fullName = "Võ Như Hiếu";
+let fullNameNoCapitalize = "võ như hiếu";
+if (fullName === fullNameNoCapitalize) {
+    console.log("Tên trùng nhau");
+} else {
+    console.log("Tên không trùng nhau");
+}
