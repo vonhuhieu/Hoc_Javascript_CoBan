7ea1ea50a59e2a0b966ba78cbb5939cb5fa791fa
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-03 07:41:36 +0700

    Giới thiệu một số hàm built-in, bắt đầu với hàm alert - tạo một thông báo đơn giản trên màn hình, nhấn OK để tắt thông báo đó

--- lesson_1/main.js
+++
@@ -1,9 +0,0 @@
-let result = "A" && "B" && "C";
-let resultBackUp = "A" || "B" || "C" || "D";
-console.log(resultBackUp);
-
-function showMessage(message) {
-    console.log(message);
-}
-
-showMessage("Hi anh em F8");
