ac5d4ece92fd5b11c1f7ae262a9df8abbb7ef2d3
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-03 09:02:48 +0700

    làm thêm vài ví dụ về toán tử gán

--- 3. Toán tử, kiểu dữ liệu/Toán tử gán/Toán tử gán_main.js
+++ 3. Toán tử, kiểu dữ liệu/Toán tử gán/Toán tử gán_main.js
@@ -1,3 +1,11 @@
 let number = 6;
 let output = ++number * 2 - number-- * 2;
 console.log(output);
+
+let a = 3;
+let result = ++a + a++ + a;
+console.log(result);
+
+let x = 5;
+let y = x++ + ++x - x--;
+console.log(y);
