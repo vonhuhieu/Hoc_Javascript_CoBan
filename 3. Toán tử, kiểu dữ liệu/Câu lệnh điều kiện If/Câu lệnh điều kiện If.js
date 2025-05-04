ab899984044db93b6d8ab9a74b8b605854604595
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-04 19:50:15 +0700

    Tìm hiểu về câu điều kiện if

---
+++ 3. Toán tử, kiểu dữ liệu/Câu lệnh điều kiện If/Câu lệnh điều kiện If.js
@@ -0,0 +1,15 @@
+// luôn trả về false
+// 0
+// false
+// '' - ""
+// undefined
+// NaN
+// null
+
+let fullName = "";
+
+if (fullName) {
+    console.log("Dieu kien dung");
+} else {
+    console.log("Dieu kien sai");
+}
