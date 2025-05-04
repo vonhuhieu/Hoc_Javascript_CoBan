4acc2c231f27f209c1c40a19e7be06863cab5514
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-04 19:57:00 +0700

    JS có 2 kiểu dữ liệu - Primitive Data && Complex Data

---
+++ 3. Toán tử, kiểu dữ liệu/Kiểu dữ liệu/Kiểu dữ liệu.js
@@ -0,0 +1,15 @@
+/*
+Kiểu dữ liệu trong JS
+
+1. Dữ liệu nguyên thủy - Primitive Data
+    - Number
+    - String
+    - Boolean
+    - Undefined
+    - Null
+    - Symbol
+
+2. Dữ liệu phức tạp - Complex Data
+    - Function
+    - Object
+ */
