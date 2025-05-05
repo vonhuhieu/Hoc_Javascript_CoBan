6ef80f279a6c793246245f9dd9b316b930ccec7e
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-05 11:22:36 +0700

    Kiểu dữ liệu chuỗi (string)

---
+++ 5. Làm việc với chuỗi/Kiểu dữ liệu chuỗi (string)/Kiểu dữ liệu chuỗi (string).js
@@ -0,0 +1,13 @@
+/*
+1. Tạo chuỗi
+    - Các cách tạo chuỗi
+    - Nên dùng cách nào? Lý do?
+    - Kiểm tra data type
+2. Một số case sử dụng backslash (\)
+3. Xem độ dài chuỗi
+4. Chú ý độ dài khi viết code
+5. Template String ES6
+*/
+
+const email = "vonhuhieu2003@gmail.com"
+console.log(email);
