a0d913d57f2ab3e200cfeb4abf5d3d47100f83fc
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-05 09:32:58 +0700

    Khái niệm hàm

---
+++ 4. Làm việc với hàm/Khái niệm hàm (function)/Khái niệm hàm (function).js
@@ -0,0 +1,23 @@
+/*
+1. Hàm?
+    - Một khối mã
+    - Làm 1 việc cụ thể
+
+2. Loại hàm
+    - Built-in
+    - Tự định nghĩa
+
+3. Tính chất
+    - Không thực thi khi định nghĩa
+    - Sẽ thực thi khi được gọi
+    - Có thể nhận tham số
+    - Có thể trả về 1 giá trị
+
+4. Tạo hàm đầu tiên
+*/
+
+function showDialog() {
+    alert("Hi xin chao");
+}
+
+showDialog();
