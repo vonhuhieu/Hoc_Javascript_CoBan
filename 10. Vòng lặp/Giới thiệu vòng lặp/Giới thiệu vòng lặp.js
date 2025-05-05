e60e13eeedd7a9a5f168b2402ea3ab931895b168
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-05 16:21:50 +0700

    Lý thuyết vòng lặp for

---
+++ 10. Vòng lặp/Giới thiệu vòng lặp/Giới thiệu vòng lặp.js
@@ -0,0 +1,7 @@
+/*
+1. for - lặp với điều kiện đúng
+2. for/in - lặp qua key của đối tượng
+3. for/of - lặp qua value của đối tượng
+4. while - lặp khi điều kiện đúng
+5. do/while - lặp ít nhất 1 lần, sau đó lặp khi điều kiện đúng
+*/
