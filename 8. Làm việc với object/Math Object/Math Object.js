50ebf40fdbb53a8f299b12b2cbbbe1b27d2b1863
vonhuhieu2003 <vonhuhieu2003@gmail.com>
2025-05-05 16:02:00 +0700

    Math object

---
+++ 8. Làm việc với object/Math Object/Math Object.js
@@ -0,0 +1,22 @@
+/*
+Math Object
+
+- Math.PI
+- Math.round()
+- Math.abs()
+- Math.ceil()
+- Math.floor()
+- Math.random()
+- Math.min()
+- Math.max()
+
+*/
+
+console.log(Math.PI);
+console.log(Math.round(1.5));
+console.log(Math.abs(-4));
+console.log(Math.ceil(3.1));
+console.log(Math.floor(5.9));
+console.log(Math.random());
+console.log(Math.min(2, 3, -5));
+console.log(Math.max(2, 3, -5));
